e0e048089efe6a0e9b73637bb757849a684dcd38
Harrison Sheldon <Harrisonsheldon@outlook.com>
2023-04-23 12:13:31 -0700

    movement

--- main.py
+++ main.py
@@ -13,14 +13,41 @@ WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("First Game!")
 
 WHITE = (255,255,255)
+BLACK = (0,0,0)
+
+BORDER = pygame.Rect(WIDTH/2 - 5 , 0, 10, HEIGHT, ) # -5 to have rect in middle of screen
 
 #how many frames to update at 
 FPS = 60
+VEL = 5 #velocity
+BULLET_VEL = 7 
 SPACESHIP_WIDTH, SPACESHIP_HEIGHT = 55, 40
+
+def yellow_handle_movement(keys_pressed, yellow):
+    #check if move off screen 
+    if keys_pressed[pygame.K_a] and yellow.x - VEL > BORDER.x + BORDER.width: #left
+        yellow.x -= VEL
+    if keys_pressed[pygame.K_d] and yellow.x + VEL + yellow.width < WIDTH: #right
+        yellow.x += VEL
+    if keys_pressed[pygame.K_s] and yellow.y + VEL + yellow.height < HEIGHT - 15: #down
+        yellow.y += VEL
+    if keys_pressed[pygame.K_w] and yellow.y - VEL > 0: #up
+        yellow.y -= VEL
+
+def red_handle_movement(keys_pressed, red):
+    if keys_pressed[pygame.K_LEFT] and red.x - VEL > BORDER.x + BORDER.width: #left
+        red.x -= VEL
+    if keys_pressed[pygame.K_RIGHT] and red.x + VEL + red.width < WIDTH: #right
+        red.x += VEL
+    if keys_pressed[pygame.K_DOWN] and red.y + VEL + red.height < HEIGHT - 15: #down
+        red.y += VEL
+    if keys_pressed[pygame.K_UP] and red.y - VEL > 0: #up
+        red.y -= VEL
 
 def draw_window(red, yellow):
     #draw on screen new color (doesnt update)
     WIN.fill((WHITE))
+    pygame.draw.rect(WIN,BLACK, BORDER)
     #blit is for text or images
     WIN.blit(YELLOW_SPACESHIP, (yellow.x, yellow.y)) #x and y from rect
     WIN.blit(RED_SPACESHIP, (red.x , red.y))
@@ -38,6 +65,8 @@ def main():
     red = pygame.Rect(700, 300, SPACESHIP_WIDTH, SPACESHIP_HEIGHT )
     yellow = pygame.Rect(100, 300, SPACESHIP_WIDTH, SPACESHIP_HEIGHT )
 
+    bullets = [] #empty list
+    
     clock = pygame.time.Clock()
     run = True
     while run: 
@@ -47,8 +76,11 @@ def main():
             if event.type == pygame.QUIT:
                 #end while loop and end game
                 run = False
-        keys_pressed = pygame.key.get_pressed
-        #test change
+
+        keys_pressed = pygame.key.get_pressed()
+        yellow_handle_movement(keys_pressed, yellow)
+        red_handle_movement(keys_pressed, red)
+        
         draw_window(red, yellow)
 
     pygame.quit() 
