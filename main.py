4b0ea35ebcb1b9deeab1c84d1a9d3a0295fe19cd
Harrison Sheldon <Harrisonsheldon@outlook.com>
2023-04-23 12:27:16 -0700

    bullets

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ BORDER = pygame.Rect(WIDTH/2 - 5 , 0, 10, HEIGHT, ) # -5 to have rect in middle 
 FPS = 60
 VEL = 5 #velocity
 BULLET_VEL = 7 
+MAX_BULLETS = 3
 SPACESHIP_WIDTH, SPACESHIP_HEIGHT = 55, 40
 
 def yellow_handle_movement(keys_pressed, yellow):
@@ -44,6 +45,13 @@ def red_handle_movement(keys_pressed, red):
     if keys_pressed[pygame.K_UP] and red.y - VEL > 0: #up
         red.y -= VEL
 
+def handel_bullets(yellow_bullets, red_bullets, yellow, red):
+    for bullet in yellow_bullets:
+        bullet.x += BULLET_VEL
+        if yellow.colliderect(bullet) # check if rect has collided
+            
+            yellow_bullets.remove(bullet)
+
 def draw_window(red, yellow):
     #draw on screen new color (doesnt update)
     WIN.fill((WHITE))
@@ -65,7 +73,8 @@ def main():
     red = pygame.Rect(700, 300, SPACESHIP_WIDTH, SPACESHIP_HEIGHT )
     yellow = pygame.Rect(100, 300, SPACESHIP_WIDTH, SPACESHIP_HEIGHT )
 
-    bullets = [] #empty list
+    red_bullets = [] #empty list
+    yellow_bullets = [] # list for each character 
     
     clock = pygame.time.Clock()
     run = True
@@ -76,10 +85,20 @@ def main():
             if event.type == pygame.QUIT:
                 #end while loop and end game
                 run = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_q and len(yellow_bullets) < MAX_BULLETS:
+                    bullet = pygame.Rect(yellow.x + yellow.width, yellow.y + yellow.height/2 - 2, 10, 5) #width of bullet 10 and height 5
+                    yellow_bullets.append(bullet)
 
+                if event.key == pygame.K_BACKSLASH and len(red_bullets) < MAX_BULLETS:
+                    bullet = pygame.Rect(red.x , red.y + red.height/2 - 2, 10, 5) #width of bullet 10 and height 5
+                    red_bullets.append(bullet)
+        
         keys_pressed = pygame.key.get_pressed()
         yellow_handle_movement(keys_pressed, yellow)
         red_handle_movement(keys_pressed, red)
+
+        handel_bullets(yellow_bullets, red_bullets, yellow, red)
         
         draw_window(red, yellow)
 
